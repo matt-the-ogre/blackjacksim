9f03044d7c5a311f2a7a349f721ed856747a7761
Matt Manuel <matt.manuel@mightyplay.com>
2014-05-16 18:04:38 -0700

    good place to stop
    TODO: fix error trying to index a hand that doesn’t exist

--- hand.js
+++ hand.js
@@ -1,6 +1,6 @@
 //---------
 // This file contains the Class definition for a Hand, which is an array of Cards
-var DEBUG = true;
+var DEBUG = false;
 
 //var Deck = require('./deck.js');
 var Card = require('./card.js');
@@ -9,7 +9,7 @@ module.exports = Hand;
 
 function Hand() {
   this.cards = [];
-  this.valueLookup = {'A':[1,11], '2':[2,2],'3':[3,3],'4':[4,4],'5':[5,5],'6':[6,6],'7':[7,7],'8':[8,8],'9':[9,9],'10':[10,10],'J':[10,10],
+  _valueLookup = {'A':[1,11], '2':[2,2],'3':[3,3],'4':[4,4],'5':[5,5],'6':[6,6],'7':[7,7],'8':[8,8],'9':[9,9],'10':[10,10],'J':[10,10],
                       'Q':[10,10],'K':[10,10]};
 
   if (DEBUG) console.log('Hand instantiated as' + this.cards);
@@ -34,16 +34,18 @@ Hand.prototype.getTotal = function() {
     //  console.log('cardTotal = ' + cardTotal);
     //});
     for (card = 0; card < this.cards.length; card++) {
-      cardTotal[0] += this.valueLookup[this.cards[card].value][0];
-      cardTotal[1] += this.valueLookup[this.cards[card].value][1];
-      console.log('cardTotal = ' + cardTotal);
+      cardTotal[0] += _valueLookup[this.cards[card].value][0];
+      cardTotal[1] += _valueLookup[this.cards[card].value][1];
+      if (DEBUG) console.log('cardTotal = ' + cardTotal);
     }
   }
   return cardTotal;
 }
 
 Hand.prototype.print = function() {
+  process.stdout.write("Hand = ");
   for (card = 0; card < this.cards.length; card++) {
-  console.log('Hand = ' + this.cards[card].value + this.cards[card].suit);
+    process.stdout.write(this.cards[card].value + this.cards[card].suit + " ");
   }
+  console.log(' ');
 }
--- test_init_deal.js
+++ test_init_deal.js
@@ -4,16 +4,77 @@ var Shoe = require('./shoe.js');
 var shoe = new Shoe(6);
 
 // set this number to 313 or higher to test that the shoe is automatically refilled with new decks
-for (i = 0; i < 10; i++) {
-  console.log(shoe.dealOneCard());
-}
+// for (i = 0; i < 10; i++) {
+//   console.log(shoe.dealOneCard());
+// }
 
 var Hand = require('./hand.js');
-var player1Hand = new Hand();
 
-for (i = 0; i < 10; i++) {
-  player1Hand.addCard(shoe.dealOneCard());
-  player1Hand.print();
-  console.log(player1Hand.getTotal());
+var hands = [];
+var numPlayers = 3;
+
+// initialize the 'hands' array
+for (hand = 0; hand <= numPlayers; hand++) {
+  hands.push(new Hand());
+}
+
+if (DEBUG) console.log(hands);
+
+// deal two cards to each player
+for (i = 0; i < 2; i++) {
+  for (hand = 0; hand < hands.length; hand++) {
+    hands[hand].addCard(shoe.dealOneCard());
+    hands[hand].print();
+    console.log(hands[hand].getTotal());
+  }
+  // TODO check if they have 21 here!
+}
+
+var gameOver = false; // start with the game not over
+var activePlayerNum = 0; // start with player 0
+
+if (DEBUG) console.log('initial deal is done, start playing the game');
+
+while(!gameOver) {
+  // deal a new card to the next player
+  if (DEBUG) console.log('Player ' + activePlayerNum + ' turn.');
+
+  hands[activePlayerNum].addCard(shoe.dealOneCard());
+  if (DEBUG) hands[activePlayerNum].print();
+  if (DEBUG) console.log(hands[activePlayerNum].getTotal());
+  // check if they bust
+  if (hands[activePlayerNum].getTotal()[0] > 21 && hands[activePlayerNum].getTotal()[1] > 21 ) {
+    // this player bust, check if it's the dealer
+    if (activePlayerNum == hands.length) {
+      // dealer busts, game over
+      if (DEBUG) console.log('Dealer bust!');
+      gameOver = true;
+    } else {
+      // player busts, take them out of the game
+      if (DEBUG) console.log('Player ' + activePlayerNum + ' busts.');
+      hands = hands.splice[activePlayerNum,1];
+      numPlayers--;
+      activePlayerNum--;
+    }
+  }
+  // check if they have 21; automatic win
+  else if (hands[activePlayerNum].getTotal()[0] == 21 || hands[activePlayerNum].getTotal()[1] == 21 ) {
+    // this player won!
+    // check if it's the dealer
+    if (activePlayerNum == hands.length) {
+      // dealer won, game over
+      if (DEBUG) console.log('Dealer won!');
+      gameOver = true;
+    } else {
+      // player wins, take them out of the game
+      if (DEBUG) console.log('Player ' + activePlayerNum + ' won!');
+      hands = hands.splice[activePlayerNum,1];
+      numPlayers--;
+      activePlayerNum--;
+    }
+  }
+  if (numPlayers == 1) gameOver = true;
+  activePlayerNum++;
+  if (activePlayerNum > numPlayers) activePlayerNum = 0; // restart at player 0
 }
 
